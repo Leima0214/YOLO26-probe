2f5f3b9472736ab665413fc9fe9aeb7471078e0c
UltralyticsAssistant <web@ultralytics.com>
2026-07-06 03:13:12 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/ESSamp.py
+++ ultralytics/nn/ESSamp.py
@@ -1,81 +1,29 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 from ultralytics.nn.modules import (
-    AIFI,
-    C1,
-    C2,
-    C2PSA,
-    C3,
-    C3TR,
-    ELAN1,
-    OBB,
-    PSA,
-    SPP,
-    SPPELAN,
-    SPPF,
-    AConv,
-    ADown,
-    Bottleneck,
-    BottleneckCSP,
-    C2f,
-    C2fAttn,
-    C2fCIB,
-    C2fPSA,
-    C3Ghost,
-    C3k2,
-    C3x,LightConv,
-    CBFuse,
-    CBLinear,
-    Classify,
-    Concat,
     Conv,
-    Conv2,
     DSConv,
-    ConvTranspose,
-    Detect,
-    DWConv,
-    DWConvTranspose2d,
-    Focus,
-    GhostBottleneck,
     GhostConv,
-    HGBlock,
-    HGStem,
-    ImagePoolingAttn,
-    Index,
-    Pose,
-    RepC3,
-    RepConv,
-    RepNCSPELAN4,
-    RepVGGDW,
-    ResNetLayer,
-    RTDETRDecoder,
-    SCDown,
-    Segment,
-    TorchVision,
-    WorldDetect,
-    v10Detect,
-    A2C2f,
-    HyperACE,
-    DownsampleConv,
-    FullPAD_Tunnel,
-    DSC3k2
 )
+
+
 class DSConv(nn.Module):
     def __init__(self, c1, c2, k=3, s=1, act=True, depth_multiplier=2):
-        super(DSConv, self).__init__()
+        super().__init__()
 
         self.block = nn.Sequential(
-            nn.Conv2d(c1, c1*depth_multiplier, kernel_size=k, stride=s, padding=k//2, groups=c1, bias=False),
+            nn.Conv2d(c1, c1 * depth_multiplier, kernel_size=k, stride=s, padding=k // 2, groups=c1, bias=False),
             nn.BatchNorm2d(c1 * depth_multiplier),
             nn.GELU() if act else nn.Identity(),
-            nn.Conv2d(c1*depth_multiplier, c2, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Conv2d(c1 * depth_multiplier, c2, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(c2),
-            nn.GELU() if act else nn.Identity()
+            nn.GELU() if act else nn.Identity(),
         )
 
     def forward(self, x):
         return self.block(x)
+
 
 class GhostConv(nn.Module):
     """Ghost Convolution https://github.com/huawei-noah/ghostnet."""
@@ -95,12 +43,10 @@ class GhostConv(nn.Module):
 
 class ESSamp(nn.Module):
     def __init__(self, c1, c2, k=3, s=1, act=True, depth_multiplier=2):
-        super(ESSamp, self).__init__()
-        self.dsconv = DSConv(c1 * 4, c2, k=k, s=s, act=act,depth_multiplier=depth_multiplier)
+        super().__init__()
+        self.dsconv = DSConv(c1 * 4, c2, k=k, s=s, act=act, depth_multiplier=depth_multiplier)
         # self.GC=GhostConv(c1=64,c2=c1)
         self.slices = nn.PixelUnshuffle(2)
-
-
 
     def forward(self, x):
         # print(x.shape)
--- ultralytics/nn/SwinTransformer.py
+++ ultralytics/nn/SwinTransformer.py
@@ -1,25 +1,29 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
- 
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-from ultralytics.nn.modules import (AIFI, C1, C2, C3, C3TR, SPP, SPPF, Bottleneck, BottleneckCSP, C2f, C3Ghost, C3x,Classify, Concat, Conv, Conv2, ConvTranspose, Detect, DWConv,DWConvTranspose2d,Focus, GhostBottleneck, GhostConv, HGBlock, HGStem, Pose, RepC3, RepConv,RTDETRDecoder, Segment,LightConv, RepConv, SpatialAttention) 
-  #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+from timm.models.layers import DropPath
+
+from ultralytics.nn.modules import (
+    Conv,
+)
+
+
+# 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
 class WindowAttention(nn.Module):
- 
-    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
- 
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0.0, proj_drop=0.0):
+
         super().__init__()
         self.dim = dim
         self.window_size = window_size  # Wh, Ww
         self.num_heads = num_heads
         head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
- 
+        self.scale = qk_scale or head_dim**-0.5
+
         # define a parameter table of relative position bias
         self.relative_position_bias_table = nn.Parameter(
-            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
- 
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads)
+        )  # 2*Wh-1 * 2*Ww-1, nH
+
         # get pair-wise relative position index for each token inside the window
         coords_h = torch.arange(self.window_size[0])
         coords_w = torch.arange(self.window_size[1])
@@ -32,29 +36,30 @@ class WindowAttention(nn.Module):
         relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
         relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
         self.register_buffer("relative_position_index", relative_position_index)
- 
+
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
- 
-        nn.init.normal_(self.relative_position_bias_table, std=.02)
+
+        nn.init.normal_(self.relative_position_bias_table, std=0.02)
         self.softmax = nn.Softmax(dim=-1)
- 
+
     def forward(self, x, mask=None):
- 
+
         B_, N, C = x.shape
         qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
- 
+
         q = q * self.scale
-        attn = (q @ k.transpose(-2, -1))
- 
+        attn = q @ k.transpose(-2, -1)
+
         relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
-            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
+            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1
+        )  # Wh*Ww,Wh*Ww,nH
         relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
         attn = attn + relative_position_bias.unsqueeze(0)
- 
+
         if mask is not None:
             nW = mask.shape[0]
             attn = attn.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
@@ -62,9 +67,9 @@ class WindowAttention(nn.Module):
             attn = self.softmax(attn)
         else:
             attn = self.softmax(attn)
- 
+
         attn = self.attn_drop(attn)
- 
+
         # print(attn.dtype, v.dtype) #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
         try:
             x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
@@ -74,12 +79,13 @@ class WindowAttention(nn.Module):
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
- 
-  #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+
+
+# 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
 class SwinTransformer(nn.Module):
     # CSP Bottleneck https://github.com/WongKinYiu/CrossStagePartialNetworks
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super(SwinTransformer, self).__init__()
+        super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
@@ -87,13 +93,13 @@ class SwinTransformer(nn.Module):
         num_heads = c_ // 32
         self.m = SwinTransformerBlock(c_, c_, num_heads, n)
         # self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
- 
+
     def forward(self, x):
         y1 = self.m(self.cv1(x))
         y2 = self.cv2(x)
         return self.cv3(torch.cat((y1, y2), dim=1))
- 
- 
+
+
 class SwinTransformerB(nn.Module):
     # CSP Bottleneck https://github.com/WongKinYiu/CrossStagePartialNetworks
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
@@ -105,14 +111,14 @@ class SwinTransformerB(nn.Module):
         num_heads = c_ // 32
         self.m = SwinTransformerBlock(c_, c_, num_heads, n)
         # self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
- 
+
     def forward(self, x):
         x1 = self.cv1(x)
         y1 = self.m(x1)
         y2 = self.cv2(x1)
         return self.cv3(torch.cat((y1, y2), dim=1))
- 
- 
+
+
 class SwinTransformerC(nn.Module):
     # CSP Bottleneck https://github.com/WongKinYiu/CrossStagePartialNetworks
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
@@ -125,16 +131,15 @@ class SwinTransformerC(nn.Module):
         num_heads = c_ // 32
         self.m = SwinTransformerBlock(c_, c_, num_heads, n)
         # self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1. #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！0) for _ in range(n)])
- 
+
     def forward(self, x):
         y1 = self.cv3(self.m(self.cv1(x)))
         y2 = self.cv2(x)
         return self.cv4(torch.cat((y1, y2), dim=1))
- 
- 
+
+
 class Mlp(nn.Module):
- 
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.SiLU, drop=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.SiLU, drop=0.0):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
@@ -142,7 +147,7 @@ class Mlp(nn.Module):
         self.act = act_layer()
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
- 
+
     def forward(self, x):
         x = self.fc1(x)
         x = self.act(x)
@@ -150,28 +155,41 @@ class Mlp(nn.Module):
         x = self.fc2(x)
         x = self.drop(x)
         return x
- 
- 
+
+
 def window_partition(x, window_size):
     B, H, W, C = x.shape
-    assert H % window_size == 0, 'feature map h and w can not divide by window size'
+    assert H % window_size == 0, "feature map h and w can not divide by window size"
     x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
     windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
     return windows
- 
- 
+
+
 def window_reverse(windows, window_size, H, W):
     B = int(windows.shape[0] / (H * W / window_size / window_size))
     x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
-    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1) #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+    x = (
+        x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    )  # 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
     return x
- 
- 
+
+
 class SwinTransformerLayer(nn.Module):
- 
-    def __init__(self, dim, num_heads, window_size=8, shift_size=0,
-                 mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0., drop_path=0.,
-                 act_layer=nn.SiLU, norm_layer=nn.LayerNorm):
+    def __init__(
+        self,
+        dim,
+        num_heads,
+        window_size=8,
+        shift_size=0,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.SiLU,
+        norm_layer=nn.LayerNorm,
+    ):
         super().__init__()
         self.dim = dim
         self.num_heads = num_heads
@@ -185,21 +203,32 @@ class SwinTransformerLayer(nn.Module):
         assert 0 <= self.shift_size < self.window_size, "shift_size must in 0-window_size"
         self.norm1 = norm_layer(dim)
         self.attn = WindowAttention(
-            dim, window_size=(self.window_size, self.window_size), num_heads=num_heads,
-            qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+            dim,
+            window_size=(self.window_size, self.window_size),
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
     def create_mask(self, H, W):
         # calculate attention mask for SW-MSA
         img_mask = torch.zeros((1, H, W, 1))  # 1 H W 1
-        h_slices = (slice(0, -self.window_size),
-                    slice(-self.window_size, -self.shift_size),
-                    slice(-self.shift_size, None))
-        w_slices = (slice(0, -self.window_size),
-                    slice(-self.window_size, -self.shift_size),
-                    slice(-self.shift_size, None))
+        h_slices = (
+            slice(0, -self.window_size),
+            slice(-self.window_size, -self.shift_size),
+            slice(-self.shift_size, None),
+        )
+        w_slices = (
+            slice(0, -self.window_size),
+            slice(-self.window_size, -self.shift_size),
+            slice(-self.shift_size, None),
+        )
         cnt = 0
         for h in h_slices:
             for w in w_slices:
@@ -208,8 +237,9 @@ class SwinTransformerLayer(nn.Module):
         mask_windows = window_partition(img_mask, self.window_size)  # nW, window_size, window_size, 1
         mask_windows = mask_windows.view(-1, self.window_size * self.window_size)
         attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
-        attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
+        attn_mask = attn_mask.masked_fill(attn_mask != 0, (-100.0)).masked_fill(attn_mask == 0, 0.0)
         return attn_mask
+
     def forward(self, x):
         # reshape x[b c h w] to x[b l c] #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
         _, _, H_, W_ = x.shape
@@ -257,7 +287,11 @@ class SwinTransformerLayer(nn.Module):
         x = x.permute(0, 2, 1).contiguous().view(-1, C, H, W)  # b c h w
         if Padding:
             x = x[:, :, :H_, :W_]  # reverse padding
-        return x #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+        return (
+            x  # 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+        )
+
+
 class SwinTransformerBlock(nn.Module):
     def __init__(self, c1, c2, num_heads, num_layers, window_size=8):
         super().__init__()
@@ -265,12 +299,23 @@ class SwinTransformerBlock(nn.Module):
         if c1 != c2:
             self.conv = Conv(c1, c2)
         # remove input_resolution
-        self.blocks = nn.Sequential(*[SwinTransformerLayer(dim=c2, num_heads=num_heads, window_size=window_size,
-                                                           shift_size=0 if (i % 2 == 0) else window_size // 2) for i in
-                                      range(num_layers)])
+        self.blocks = nn.Sequential(
+            *[
+                SwinTransformerLayer(
+                    dim=c2,
+                    num_heads=num_heads,
+                    window_size=window_size,
+                    shift_size=0 if (i % 2 == 0) else window_size // 2,
+                )
+                for i in range(num_layers)
+            ]
+        )
+
     def forward(self, x):
         if self.conv is not None:
             x = self.conv(x)
         x = self.blocks(x)
         return x
- #详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+
+
+# 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
