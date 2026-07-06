03c7556cb95bcd8e59ef6ad87779abbca9d2fcde
UltralyticsAssistant <web@ultralytics.com>
2026-07-06 03:14:57 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/BoTNet.py
+++ ultralytics/nn/BoTNet.py
@@ -1,36 +1,34 @@
-#详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽 
-import contextlib
-from copy import deepcopy
-from pathlib import Path
+# 详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽
+
+import math
 
 import torch
 import torch.nn as nn
 
-import math
-#详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽 
-import numpy as np
-#详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽 
-from ultralytics.nn.modules import (AIFI, C1, C2, C3, C3TR, SPP, SPPF, Bottleneck, BottleneckCSP, C2f, C3Ghost, C3x,
-                                    Classify, Concat, Conv, Conv2, ConvTranspose, Detect, DWConv, DWConvTranspose2d,
-                                    Focus, GhostBottleneck, GhostConv, HGBlock, HGStem, Pose, RepC3, RepConv,
-                                    RTDETRDecoder, Segment)
-#详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽 
+# 详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽
+# 详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽
+from ultralytics.nn.modules import Conv
+
+
+# 详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽
 class MHSA(nn.Module):
     def __init__(self, n_dims, width=14, height=14, heads=4, pos_emb=False):
-        super(MHSA, self).__init__()
- 
+        super().__init__()
+
         self.heads = heads
         self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
         self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
         self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
         self.pos = pos_emb
         if self.pos:
-            self.rel_h_weight = nn.Parameter(torch.randn([1, heads, (n_dims) // heads, 1, int(height)]),
-                                             requires_grad=True)
-            self.rel_w_weight = nn.Parameter(torch.randn([1, heads, (n_dims) // heads, int(width), 1]),
-                                             requires_grad=True)
+            self.rel_h_weight = nn.Parameter(
+                torch.randn([1, heads, (n_dims) // heads, 1, int(height)]), requires_grad=True
+            )
+            self.rel_w_weight = nn.Parameter(
+                torch.randn([1, heads, (n_dims) // heads, int(width), 1]), requires_grad=True
+            )
         self.softmax = nn.Softmax(dim=-1)
- 
+
     def forward(self, x):
         n_batch, C, width, height = x.size()
         q = self.query(x).view(n_batch, self.heads, C // self.heads, -1)
@@ -39,16 +37,24 @@ class MHSA(nn.Module):
         # print('q shape:{},k shape:{},v shape:{}'.format(q.shape,k.shape,v.shape))  #1,4,64,256
         content_content = torch.matmul(q.permute(0, 1, 3, 2), k)  # 1,C,h*w,h*w
         # print("qkT=",content_content.shape)
-        c1, c2, c3, c4 = content_content.size()
+        _c1, _c2, c3, _c4 = content_content.size()
         if self.pos:
             # print("old content_content shape",content_content.shape) #1,4,256,256
-            content_position = (self.rel_h_weight + self.rel_w_weight).view(1, self.heads, C // self.heads, -1).permute(
-                0, 1, 3, 2)  # 1,4,1024,64
- 
+            content_position = (
+                (self.rel_h_weight + self.rel_w_weight).view(1, self.heads, C // self.heads, -1).permute(0, 1, 3, 2)
+            )  # 1,4,1024,64
+
             content_position = torch.matmul(content_position, q)  # ([1, 4, 1024, 256])
-            content_position = content_position if (
-                        content_content.shape == content_position.shape) else content_position[:, :, :c3, ]
-            assert (content_content.shape == content_position.shape)
+            content_position = (
+                content_position
+                if (content_content.shape == content_position.shape)
+                else content_position[
+                    :,
+                    :,
+                    :c3,
+                ]
+            )
+            assert content_content.shape == content_position.shape
             # print('new pos222-> shape:',content_position.shape)
             # print('new content222-> shape:',content_content.shape)
             energy = content_content + content_position
@@ -58,14 +64,14 @@ class MHSA(nn.Module):
         out = torch.matmul(v, attention.permute(0, 1, 3, 2))  # 1,4,256,64
         out = out.view(n_batch, C, width, height)
         return out
- 
- 
+
+
 class BottleneckTransformer(nn.Module):
     # Transformer bottleneck
     # expansion = 1
- 
+
     def __init__(self, c1, c2, stride=1, heads=4, mhsa=True, resolution=None, expansion=1):
-        super(BottleneckTransformer, self).__init__()
+        super().__init__()
         c_ = int(c2 * expansion)
         self.cv1 = Conv(c1, c_, 1, 1)
         # self.bn1 = nn.BatchNorm2d(c2)
@@ -80,11 +86,10 @@ class BottleneckTransformer(nn.Module):
         self.shortcut = c1 == c2
         if stride != 1 or c1 != expansion * c2:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(c1, expansion * c2, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(expansion * c2)
+                nn.Conv2d(c1, expansion * c2, kernel_size=1, stride=stride), nn.BatchNorm2d(expansion * c2)
             )
         self.fc1 = nn.Linear(c2, c2)
- 
+
     def forward(self, x):
         out = x + self.cv2(self.cv1(x)) if self.shortcut else self.cv2(self.cv1(x))
         return out
@@ -94,30 +99,26 @@ class BottleneckTransformer(nn.Module):
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
-import numpy as np
-import torch
-from torch import nn
-
-from torch.nn import init
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
-import torch
 import torch.nn as nn
-import math
 from einops import rearrange
-
+from torch import nn
 
 # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 
 
 class AKConv(nn.Module):
     def __init__(self, inc, outc, num_param, stride=1, bias=None):
-        super(AKConv, self).__init__()
+        super().__init__()
         self.num_param = num_param
         self.stride = stride
-        self.conv = nn.Sequential(nn.Conv2d(inc, outc, kernel_size=(num_param, 1), stride=(num_param, 1), bias=bias),
-                                  nn.BatchNorm2d(outc), nn.SiLU())
+        self.conv = nn.Sequential(
+            nn.Conv2d(inc, outc, kernel_size=(num_param, 1), stride=(num_param, 1), bias=bias),
+            nn.BatchNorm2d(outc),
+            nn.SiLU(),
+        )
         self.p_conv = nn.Conv2d(inc, 2 * num_param, kernel_size=3, padding=1, stride=stride)
         nn.init.constant_(self.p_conv.weight, 0)
         self.p_conv.register_full_backward_hook(self._set_lr)
@@ -141,10 +142,12 @@ class AKConv(nn.Module):
         q_lt = p.detach().floor()
         q_rb = q_lt + 1
 
-        q_lt = torch.cat([torch.clamp(q_lt[..., :N], 0, x.size(2) - 1), torch.clamp(q_lt[..., N:], 0, x.size(3) - 1)],
-                         dim=-1).long()
-        q_rb = torch.cat([torch.clamp(q_rb[..., :N], 0, x.size(2) - 1), torch.clamp(q_rb[..., N:], 0, x.size(3) - 1)],
-                         dim=-1).long()
+        q_lt = torch.cat(
+            [torch.clamp(q_lt[..., :N], 0, x.size(2) - 1), torch.clamp(q_lt[..., N:], 0, x.size(3) - 1)], dim=-1
+        ).long()
+        q_rb = torch.cat(
+            [torch.clamp(q_rb[..., :N], 0, x.size(2) - 1), torch.clamp(q_rb[..., N:], 0, x.size(3) - 1)], dim=-1
+        ).long()
         q_lb = torch.cat([q_lt[..., :N], q_rb[..., N:]], dim=-1)
         q_rt = torch.cat([q_rb[..., :N], q_lt[..., N:]], dim=-1)
 
@@ -164,10 +167,12 @@ class AKConv(nn.Module):
         x_q_rt = self._get_x_q(x, q_rt, N)
 
         # bilinear
-        x_offset = g_lt.unsqueeze(dim=1) * x_q_lt + \
-                   g_rb.unsqueeze(dim=1) * x_q_rb + \
-                   g_lb.unsqueeze(dim=1) * x_q_lb + \
-                   g_rt.unsqueeze(dim=1) * x_q_rt
+        x_offset = (
+            g_lt.unsqueeze(dim=1) * x_q_lt
+            + g_rb.unsqueeze(dim=1) * x_q_rb
+            + g_lb.unsqueeze(dim=1) * x_q_lb
+            + g_rt.unsqueeze(dim=1) * x_q_rt
+        )
 
         x_offset = self._reshape_x_offset(x_offset, self.num_param)
         out = self.conv(x_offset)
@@ -179,15 +184,11 @@ class AKConv(nn.Module):
         base_int = round(math.sqrt(self.num_param))
         row_number = self.num_param // base_int
         mod_number = self.num_param % base_int
-        p_n_x, p_n_y = torch.meshgrid(
-            torch.arange(0, row_number),
-            torch.arange(0, base_int))
+        p_n_x, p_n_y = torch.meshgrid(torch.arange(0, row_number), torch.arange(0, base_int))
         p_n_x = torch.flatten(p_n_x)
         p_n_y = torch.flatten(p_n_y)
         if mod_number > 0:
-            mod_p_n_x, mod_p_n_y = torch.meshgrid(
-                torch.arange(row_number, row_number + 1),
-                torch.arange(0, mod_number))
+            mod_p_n_x, mod_p_n_y = torch.meshgrid(torch.arange(row_number, row_number + 1), torch.arange(0, mod_number))
 
             mod_p_n_x = torch.flatten(mod_p_n_x)
             mod_p_n_y = torch.flatten(mod_p_n_y)
@@ -198,8 +199,8 @@ class AKConv(nn.Module):
 
     def _get_p_0(self, h, w, N, dtype):
         p_0_x, p_0_y = torch.meshgrid(
-            torch.arange(0, h * self.stride, self.stride),
-            torch.arange(0, w * self.stride, self.stride))
+            torch.arange(0, h * self.stride, self.stride), torch.arange(0, w * self.stride, self.stride)
+        )
 
         p_0_x = torch.flatten(p_0_x).view(1, 1, h, w).repeat(1, N, 1, 1)
         p_0_y = torch.flatten(p_0_y).view(1, 1, h, w).repeat(1, N, 1, 1)
@@ -235,15 +236,16 @@ class AKConv(nn.Module):
 
     @staticmethod
     def _reshape_x_offset(x_offset, num_param):
-        b, c, h, w, n = x_offset.size()
+        _b, _c, _h, _w, _n = x_offset.size()
         # using Conv3d
         # x_offset = x_offset.permute(0,1,4,2,3), then Conv3d(c,c_out, kernel_size =(num_param,1,1),stride=(num_param,1,1),bias= False)
         # using 1 × 1 Conv
         # x_offset = x_offset.permute(0,1,4,2,3), then, x_offset.view(b,c×num_param,h,w)  finally, Conv2d(c×num_param,c_out, kernel_size =1,stride=1,bias= False)
         # using the column conv as follow， then, Conv2d(inc, outc, kernel_size=(num_param, 1), stride=(num_param, 1), bias=bias)
 
-        x_offset = rearrange(x_offset, 'b c h w n -> b c (h n) w')
+        x_offset = rearrange(x_offset, "b c h w n -> b c (h n) w")
         return x_offset
+
     # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 
     # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
@@ -253,7 +255,7 @@ class AKConv(nn.Module):
 
 class ChannelAttentionModule(nn.Module):
     def __init__(self, c1, reduction=16):
-        super(ChannelAttentionModule, self).__init__()
+        super().__init__()
         mid_channel = c1 // reduction
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
@@ -261,7 +263,7 @@ class ChannelAttentionModule(nn.Module):
         self.shared_MLP = nn.Sequential(
             nn.Linear(in_features=c1, out_features=mid_channel),
             nn.LeakyReLU(0.1, inplace=True),
-            nn.Linear(in_features=mid_channel, out_features=c1)
+            nn.Linear(in_features=mid_channel, out_features=c1),
         )
         self.act = nn.Sigmoid()
         # self.act=nn.SiLU()
@@ -276,7 +278,7 @@ class ChannelAttentionModule(nn.Module):
 
 class SpatialAttentionModule(nn.Module):
     def __init__(self):
-        super(SpatialAttentionModule, self).__init__()
+        super().__init__()
         # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
         self.conv2d = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
         # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
@@ -286,14 +288,16 @@ class SpatialAttentionModule(nn.Module):
         avgout = torch.mean(x, dim=1, keepdim=True)
         maxout, _ = torch.max(x, dim=1, keepdim=True)
         out = torch.cat([avgout, maxout], dim=1)
-        out = self.act(self.conv2d(out))  # 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
+        out = self.act(
+            self.conv2d(out)
+        )  # 详细改进流程和操作，请关注B站博主：Ai学术叫叫兽 er,畅享一对一指点迷津，已指导无数家人拿下学术硕果！！！
         return out
         # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 
 
 class CBAM(nn.Module):
     def __init__(self, c1, c2):
-        super(CBAM, self).__init__()
+        super().__init__()
         self.channel_attention = ChannelAttentionModule(c1)
         self.spatial_attention = SpatialAttentionModule()
         # self.AK=AKConv(inc=256,outc=256,num_param=3)
@@ -303,22 +307,29 @@ class CBAM(nn.Module):
         out = self.channel_attention(x) * x
         out = self.spatial_attention(out) * out
         return out
+
     # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
 
     # 详细改进流程和操作，请关注B站博主：AI学术叫叫兽
+
+
 class BoTNet(nn.Module):
     # CSP Bottleneck with 3 convolutions
     def __init__(self, c1, c2, n=1, e=0.5, e2=1, w=20, h=20):  # ch_in, ch_out, number, , expansion,w,h
-        super(BoTNet, self).__init__()
+        super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
         self.m = nn.Sequential(
-            *[BottleneckTransformer(c_, c_, stride=1, heads=4, mhsa=True, resolution=(w, h), expansion=e2) for _ in
-              range(n)])
+            *[
+                BottleneckTransformer(c_, c_, stride=1, heads=4, mhsa=True, resolution=(w, h), expansion=e2)
+                for _ in range(n)
+            ]
+        )
         # self.CBAM=CBAM(c1,c2)
-#详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽  
+
+    # 详细的各类改进方法和流程操作，请关注B站博主：AI学术叫叫兽
     def forward(self, x):
         # x=self.CBAM(x)
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
